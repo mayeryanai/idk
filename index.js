335c662b2d304832fd323b9014d9f34c84c43bda
mayeryanai <mayeryanai@gmail.com>
2024-10-08 18:07:45 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+let total=document.getElementById('total')
+
+let amount=0
+
+function take(){
+    amount=amount-1
+    total.innerHTML=amount
+}
+
+function add(){
+amount=amount+1
+total.innerHTML=amount
+}
+
+
+
+//+חיבור
+//-חיסור
+//*כפל
+//**חסקה 
+//--להוריד ב1
+//++לעלות ב1
+//%שארית חלוקה
+
